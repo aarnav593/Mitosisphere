a49c8cd4b7724b4e28effd7d4fa0bded64565323
Pellucid1137 <pellucid1137@gmail.com>
2025-01-07 09:06:25 -0500

    update site domain in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mitosisphere.netlify.app",
+  site: "https://mitosisphere.org",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
